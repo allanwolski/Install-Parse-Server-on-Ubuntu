0df86605d9dd93260290c9ade4baa6954077ea80
Allan Wolski <allanmw@gmail.com>
2020-01-20 19:01:05 -0300

    Update easy-install.sh

--- easy-install.sh
+++ easy-install.sh
@@ -16,7 +16,7 @@ echo 'installing Mongo DB';
 sleep 2;
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 apt-get update
 apt-get install -y mongodb-org
 service mongod start
